11dccfdebdd94103007406ea0c6bee69c25a6b79
HKEDU <HKEDU@DESKTOP-606IM62>
2024-04-19 10:59:57 +0900

    점주/식당 등록

---
+++ deliverySpringProject/src/main/java/deliverySpringProject/controller/OwnerController.java
@@ -0,0 +1,14 @@
+package deliverySpringProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("owner")
+public class OwnerController {
+	@GetMapping("ownerRegist")
+	public String ownerRegist() {
+		return "thymeleaf/owner/ownerRegist";
+	}
+}
