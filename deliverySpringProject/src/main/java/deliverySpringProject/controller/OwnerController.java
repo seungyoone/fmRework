d1103752d6d42e7c30031ea1e494ee286a9d337a
user <user@DESKTOP-EBGRIIM>
2024-04-19 11:41:24 +0900

    autowired

--- deliverySpringProject/src/main/java/deliverySpringProject/controller/OwnerController.java
+++ deliverySpringProject/src/main/java/deliverySpringProject/controller/OwnerController.java
@@ -1,5 +1,6 @@
 package deliverySpringProject.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import deliverySpringProject.service.owner.ShopRegistService;
 
+
+
 @Controller
 @RequestMapping("owner")
 public class OwnerController {
+	
+	@Autowired
+	ShopRegistService shopRegistService;
 
 	@GetMapping("ownerRegist")
 	public String ownerRegist() {
