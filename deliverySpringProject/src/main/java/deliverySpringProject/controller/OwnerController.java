6014a38f85842d1b5b6635705de6057822f1f305
user <user@DESKTOP-EBGRIIM>
2024-04-19 11:32:04 +0900

    수정완료

--- deliverySpringProject/src/main/java/deliverySpringProject/controller/OwnerController.java
+++ deliverySpringProject/src/main/java/deliverySpringProject/controller/OwnerController.java
@@ -1,12 +1,18 @@
 package deliverySpringProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import deliverySpringProject.service.owner.ShopRegistService;
+
 @Controller
 @RequestMapping("owner")
 public class OwnerController {
+	@Autowired
+	ShopRegistService shopRegistService;
+	
 	@GetMapping("ownerRegist")
 	public String ownerRegist() {
 		return "thymeleaf/owner/ownerRegist";
--- deliverySpringProject/src/main/java/deliverySpringProject/service/ShopRegistService.java
+++
@@ -1,5 +0,0 @@
-package deliverySpringProject.service;
-
-public class ShopRegistService {
-
-}
